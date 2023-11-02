b09df7fe97e8dc46e148f6dba08f67521a5edb39
Pedro Henrique <pedrosilv1512@gmail.com>
2023-11-02 14:33:06 -0300

    Create termometro.py

---
+++ Funções/termometro.py
@@ -0,0 +1,10 @@
+# Conversão temperatura
+
+def fahrenheit(n):
+    calculo_fahrenheit = (1.8 * n) +32
+    print(f'Fahrenheit: {calculo_fahrenheit:.2f}')
+
+def kelvin(n):
+    calculo_kelvin = n + 273.15
+    print(f'Kelvin: {calculo_kelvin:.2f}')
+
