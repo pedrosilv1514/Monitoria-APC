7af04ac79698eeaa0b2e2a3be43004d1c43c09fa
Pedro Henrique <pedrosilv1512@gmail.com>
2023-11-02 15:53:43 -0300

    Create cinco_patinhos.py

---
+++ Funções/cinco_patinhos.py
@@ -0,0 +1,13 @@
+def refrao(n):
+    return n - 1
+
+def estrofe(n):
+    print(f'{n} patinhos')
+    print('Foram passear')
+    print('Além das montanhas')
+    print('Para brincar')
+    print('A mamãe gritou')
+    print('Quá, quá, quá, quá!')
+    print(f'Mas só {refrao(n)} patinhos')
+    print('Voltaram de lá')
+    print('Agora só falta aprender a fazer loop!')
