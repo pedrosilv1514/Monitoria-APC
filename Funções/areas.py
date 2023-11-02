750918082dfe5d5249de0b8f45cdcbe9d9d0a4c8
Pedro Henrique <pedrosilv1512@gmail.com>
2023-11-02 15:38:59 -0300

    Create areas.py

---
+++ Funções/areas.py
@@ -0,0 +1,11 @@
+def areas(a,b,c):
+    retangulo = int(a*b)
+    triangulo = int((b*c)/2)
+    trapezio = int(((c+b)*a)/2)
+
+    print(retangulo)
+    print(triangulo)
+    print(trapezio)
+
+
+areas(1,2,3)
